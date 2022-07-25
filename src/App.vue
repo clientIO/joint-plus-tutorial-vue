d6b0edb45649ccff7251b7a138aba98ffcae3cae
James Williams <jamesgeorgewilliams@protonmail.com>
2022-07-25 12:18:55 +0200

    vue: add vue 2 src code

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,80 @@
 <template>
-  <div class="canvas" ref="canvas"></div>
+    <div class="canvas" ref="canvas"></div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Component, Vue } from 'vue-property-decorator';
 import { dia, ui, shapes } from '@clientio/rappid';
 
-@Options({})
+@Component({})
 export default class App extends Vue {
-  declare public $refs: {
-   canvas: HTMLDivElement;
-  }
 
-  private graph: dia.Graph;
-  private paper: dia.Paper;
-  private scroller: ui.PaperScroller;
+    declare public $refs: {
+        canvas: HTMLDivElement;
+    }
+    private graph: dia.Graph;
+    private paper: dia.Paper;
+    private scroller: ui.PaperScroller;
 
-  public created(): void {
-    const graph = this.graph = new dia.Graph({}, { cellNamespace: shapes });
+    public created(): void {
+        const graph = this.graph = new dia.Graph({}, { cellNamespace: shapes });
 
-    const paper = this.paper = new dia.Paper({
-        model: graph,
-        background: {
-            color: '#F8F9FA',
-        },
-        frozen: true,
-        async: true,
-        sorting: dia.Paper.sorting.APPROX,
-        cellViewNamespace: shapes
-    });
+        const paper = this.paper = new dia.Paper({
+            model: graph,
+            background: {
+                color: '#F8F9FA',
+            },
+            frozen: true,
+            async: true,
+            sorting: dia.Paper.sorting.APPROX,
+            cellViewNamespace: shapes
+        });
 
-    const scroller = this.scroller = new ui.PaperScroller({
-        paper,
-        autoResizePaper: true,
-        cursor: 'grab'
-    });
+        const scroller = this.scroller = new ui.PaperScroller({
+            paper,
+            autoResizePaper: true,
+            cursor: 'grab'
+        });
 
-    scroller.render();
+        scroller.render();
 
-    const rect = new shapes.standard.Rectangle({
-        position: { x: 100, y: 100 },
-        size: { width: 100, height: 50 },
-        attrs: {
-            label: {
-                text: 'Hello World'
+        const rect = new shapes.standard.Rectangle({
+            position: { x: 100, y: 100 },
+            size: { width: 100, height: 50 },
+            attrs: {
+                label: {
+                    text: 'Hello World'
+                }
             }
-        }
-    });
+        });
 
-    this.graph.addCell(rect);
-  }
+        this.graph.addCell(rect);
+    }
 
-  public mounted(): void {
-    const { scroller, paper, $refs : { canvas } } = this;
-    canvas.appendChild(this.scroller.el);
-    scroller.center();
-    paper.unfreeze();
-  }
+    public mounted(): void {
+        const { scroller, paper, $refs : { canvas } } = this;
+        canvas.appendChild(this.scroller.el);
+        scroller.center();
+        paper.unfreeze();
+    }
 }
 </script>
 
 <style lang="scss">
-  @import "~@clientio/rappid/rappid.css";
+@import "~@clientio/rappid/rappid.css";
 
-  body {
-  height: 100vh;
-  box-sizing: border-box;
-  margin: 0;
+body {
+    height: 100vh;
+    box-sizing: border-box;
+    margin: 0;
 
-    .canvas {
-      width: 100%;
-      height: 100%;
+.canvas {
+    width: 100%;
+    height: 100%;
 
-      .joint-paper {
-          border: 1px solid #A0A0A0;
-      }
+    .joint-paper {
+        border: 1px solid #A0A0A0;
     }
-  }
+ }
+}
 </style>
