892556557ea24559ad851fe133f71017b90bcfa0
Martin Kaněra <50818204+MartinKanera@users.noreply.github.com>
2023-09-19 08:37:53 +0200

    feat: new demo for Vue integration tutorial (#6)

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,61 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { dia, ui, shapes } from '@clientio/rappid/rappid.js';
+
+const canvas = ref<Element | null>(null);
+
+const graph = new dia.Graph({}, {
+  cellNamespace: shapes
+});
+const paper = new dia.Paper({
+  model: graph,
+  background: {
+    color: '#F8F9FA'
+  },
+  frozen: true,
+  async: true,
+  sorting: dia.Paper.sorting.APPROX,
+  cellViewNamespace: shapes
+});
+
+const scroller = new ui.PaperScroller({
+  paper,
+  autoResizePaper: true,
+  cursor: 'grab'
+});
+
+scroller.render();
+
+const rect = new shapes.standard.Rectangle({
+  position: { x: 100, y: 100 },
+  size: { width: 100, height: 50 },
+  attrs: {
+    label: {
+      text: 'Hello World'
+    }
+  }
+});
+
+graph.addCell(rect);
+
+onMounted(() => {
+  canvas.value?.appendChild(scroller.el);
+  scroller.center();
+  paper.unfreeze();
+});
+</script>
+
 <template>
   <div class="canvas" ref="canvas"></div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import { dia, ui, shapes } from '@clientio/rappid';
-
-@Options({})
-export default class App extends Vue {
-  declare public $refs: {
-   canvas: HTMLDivElement;
-  }
-
-  private graph: dia.Graph;
-  private paper: dia.Paper;
-  private scroller: ui.PaperScroller;
-
-  public created(): void {
-    const graph = this.graph = new dia.Graph({}, { cellNamespace: shapes });
-
-    const paper = this.paper = new dia.Paper({
-        model: graph,
-        background: {
-            color: '#F8F9FA',
-        },
-        frozen: true,
-        async: true,
-        sorting: dia.Paper.sorting.APPROX,
-        cellViewNamespace: shapes
-    });
-
-    const scroller = this.scroller = new ui.PaperScroller({
-        paper,
-        autoResizePaper: true,
-        cursor: 'grab'
-    });
-
-    scroller.render();
-
-    const rect = new shapes.standard.Rectangle({
-        position: { x: 100, y: 100 },
-        size: { width: 100, height: 50 },
-        attrs: {
-            label: {
-                text: 'Hello World'
-            }
-        }
-    });
-
-    this.graph.addCell(rect);
-  }
-
-  public mounted(): void {
-    const { scroller, paper, $refs : { canvas } } = this;
-    canvas.appendChild(this.scroller.el);
-    scroller.center();
-    paper.unfreeze();
-  }
+<style scoped>
+.canvas {
+  width: 100%;
+  height: 100%;
 }
-</script>
 
-<style lang="scss">
-  @import "~@clientio/rappid/rappid.css";
-
-  body {
-  height: 100vh;
-  box-sizing: border-box;
-  margin: 0;
-
-    .canvas {
-      width: 100%;
-      height: 100%;
-
-      .joint-paper {
-          border: 1px solid #A0A0A0;
-      }
-    }
-  }
+.canvas:deep(.joint-paper) {
+  border: 1px solid #a0a0a0;
+}
 </style>
